a5915563f0ab67df85873eed536a56c91af08c12
kakapreter <85819721+kakapreter@users.noreply.github.com>
2023-10-16 18:57:46 +0800

    Update nnshop.sql

--- sql/nnshop.sql
+++ sql/nnshop.sql
@@ -63,10 +63,11 @@ CREATE TABLE `nna_admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `nna_admin` VALUES ('1', 'jack', '1234567', 'job', '2555@qq.com', '12345678909', null, null);
+
 -- ----------------------------
 -- Records of nna_admin
 -- ----------------------------
+INSERT INTO `nna_admin` VALUES ('1', 'jack', '1234567', 'job', '2555@qq.com', '12345678909', null, null);
 
 -- ----------------------------
 -- Table structure for nna_admin_role
